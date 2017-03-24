c46b3786ae556b8e9247a74cc939e80a3eb1a1e4
rmcpeck <rmcpeck@uncc.edu>
2017-03-24 15:23:02 +0000

    part 2 complete

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -3,8 +3,6 @@
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     Movie.create movie
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
   end
 end
 
@@ -22,13 +20,23 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(',').each do |rating|
+    if uncheck
+      uncheck "ratings_#{rating}"
+    else
+      check "ratings_#{rating}"
+    end
+  end
 end
 
 Then /I should see all the movies/ do
-  # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  movies = Movie.all
+  
+  if movies.size == 10
+    movies.each do |movie|
+      assert page.body =~ /#{movie.title}/m, "#{movie.title} did not appear"
+    end
+  else
+    false
+  end
 end
